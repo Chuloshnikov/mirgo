e1a40cb938b27c688760396d74ab39456cf42e0a
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-11-14 10:41:17 +0200

    dene

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            <div>logo</div>
+            <div>searchInput</div>
+            <div>navBarList</div>
+        </nav>
+    </header>
+  )
+}
+
+export default Header;
