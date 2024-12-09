745693b6ebf3bdc4f2d5b2c80ca113e875124a28
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-12-10 01:03:34 +0200

    +

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,17 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+        "2x1": "1400px",
+      },
       colors: {
         lightOrange: "#FE6E44",
         darkOrange: "rgb(255,77,17)",
