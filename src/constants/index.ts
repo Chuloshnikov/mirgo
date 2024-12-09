787060bb3d4dc2f3e3f9527b0ee7a3e5e573bdcd
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-12-10 01:43:19 +0200

    footer done

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -16,3 +16,78 @@ export const navBarList = [
         link: "/",
     },
 ]
+
+export const footerData = [
+    {
+        _id: 2221,
+        title: "Get to Know Us",
+        listItem: [
+            {
+                _id: "001",
+                listData: [
+                    "Careers",
+                    "Blog",
+                    "About Mirago",
+                    "Investor Relations",
+                    "Mirago Devices",
+                    "Mirago Science"
+                ]
+            },
+           
+        ]
+    },
+    {
+        _id:2222,
+        title: "Make Money with Us",
+        listItem: [
+            {
+                _id: "002",
+                listData: [
+                    "Sell products on Mirago",
+                    "Sell on Mirago Business",
+                    "Sell apps on Mirago",
+                    "Become an Affiliate",
+                    "Advertise Your Products",
+                    "Sell Product with Us",
+                    "Host an Shoppers Hub",
+                    "See More Make Money with Us"
+                ]
+            },
+        ]
+    },
+    {
+        _id:2223,
+        title: "Mirago Payment Products",
+        listItem: [
+            {
+                _id: "003",
+                listData: [
+                    "Mirago Busines Card",
+                    "Shop with Points",
+                    "Reload Your Balance",
+                    "Mirago Currency Converter",
+                ]
+            },
+        ]
+    },
+    {
+        _id: 2224,
+        title: "Let Us Help You",
+        listItem: [
+            {
+                _id: "004",
+                listData: [
+                    "Mirago and COVID-19",
+                    "Your Account",
+                    "Your Orders",
+                    "Shipping Rates & Policies",
+                    "Returns & Replacements",
+                    "Manage Your Content and Devices",
+                    "Mirago Assistants",
+                    "FAQ & Help"
+                ]
+            }
+        ]
+    }
+]
+
