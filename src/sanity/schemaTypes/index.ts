ec5a052d057f918142f8c09688b566520f54e28f
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-12-10 15:56:40 +0200

    sckemas added

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import banner from '../schemas/banner'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   //banner, products, category
-  types: [],
+  types: [banner],
 }
