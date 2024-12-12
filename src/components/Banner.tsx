a97859b68ebb095ada314f1fea054dd785e7064e
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-12-12 14:42:39 +0200

    +

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ import { getBannersData } from '@/lib/getData';
 import React from 'react'
 
 const Banner = async () => {
-    const danners = await getBannersData();
+    const banners = await getBannersData();
     console.log(banners);
   return (
     <div>Banner</div>
