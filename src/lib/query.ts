af2bbceb31eeb32cac732b28289d513037fd0b85
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-12-12 13:25:28 +0200

    +

--- src/lib/query.ts
+++ src/lib/query.ts
@@ -2,14 +2,14 @@ import { groq } from "next-sanity";
 
 const bannerQuery = groq`*[_type == 'banner']{
 ...
-}|other(_createdAt asc)`; 
+}|order(_createdAt asc)`; 
 
 const productsQuery = groq`*[_type == 'product']{
 ...
-}|other(_createdAt asc)`; 
+}|order(_createdAt asc)`; 
 
 const bestSellersQuery = groq`*[_type == 'product' && position == 'Bestsellers']{
 ...
-}|other(_createdAt asc)`; 
+}|order(_createdAt asc)`; 
 
 export { bannerQuery, productsQuery, bestSellersQuery };
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,7 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import banner from '../schemas/banner'
-import products from '../schemas/products'
-import category from '../schemas/category'
+import products from './products'
+import category from './category'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [banner, products, category],
