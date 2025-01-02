216022b6c3dcfad23dae63862a0e0e0f2bb64850
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-01-02 13:46:24 +0200

    done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,64 @@
-import { configureStore } from '@reduxjs/toolkit';
-import miragoReducer from './miragoSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import miragoReducer from "./miragoSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  WebStorage,
+} from "redux-persist";
+import createWebStorage from "redux-persist/es/storage/createWebStorage";
+
+export function createPersistStore(): WebStorage {
+  const isServer = typeof window === "undefined";
+
+  // will returns our dummy server
+  if (isServer) {
+    return {
+      getItem() {
+        return Promise.resolve(null);
+      },
+      setItem() {
+        return Promise.resolve();
+      },
+      removeItem() {
+        return Promise.resolve();
+      },
+    };
+  }
+  return createWebStorage("local");
+}
+
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("local")
+    : createPersistStore();
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, miragoReducer);
 
 export const store = configureStore({
   reducer: {
-    mirago: miragoReducer,
+    mirago: persistedReducer,
   },
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
