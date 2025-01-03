08d2f8283c6275c4e3a65163cf7a9900ccb1dba6
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-01-03 16:29:34 +0200

    done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ import { HiMenuAlt2 } from 'react-icons/hi';
 
 const Header = () => {
   return (
-    <header className='w-full h-20 bg-accentWhite border-b-[1px] border-b-lightText pt-4'>
+    <header className='w-full h-20 bg-accentWhite border-b-[1px] border-b-lightText pt-4 sticky z-50 top-0 left-0'>
         <nav>
             <Container className='h-full flex items-center justify-between gap-5 lg:gap-10'>
                 <Logo/>
