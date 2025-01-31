f1f2b5ee142e61e36e8abd020c404a0595104872
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-01-31 16:55:57 +0200

    credentials form and github button added

--- src/app/(user)/sign-in/page.tsx
+++ src/app/(user)/sign-in/page.tsx
@@ -1,10 +1,12 @@
 import { auth, signIn } from '@/auth';
 import Container from '@/components/Container'
+import { SignInForm } from '@/components/SignInForm';
 import { redirect } from 'next/navigation';
 
 import React from 'react';
 
 import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa6";
 
 
 export default async function SignIn() {
@@ -14,21 +16,39 @@ export default async function SignIn() {
     redirect('/');
   }
   return (
-    <Container className="py-20 flex flex-col justify-center items-center h-[400px]">
-        <form 
-        action={async () => {
-          'use server';
-          await signIn('google', {redirectTo: "/"})
-        }}
-        className="flex items-center gap-1 border border-blue-500 
-        font-semibold bg-blue-50 px-4 py-1.5 hover:bg-blue-800
-        hover:text-white hoverEffect"
-        >
-            <button className='flex gap-2 items-center'>
-              Sign in with Google
-              <FcGoogle />
-            </button>
-        </form>
+    <Container className="py-20 flex flex-col justify-center items-center">
+       <div className="w-[500px] bg-bgLight p-10 rounded-lg shadow-sm shadow-darkRed/50">
+          <div className="mb-5">
+          <h2 className="text-xl font-bold">Oauth sign in</h2>
+            <div className="flex items-center gap-3 mt-2">
+              <form 
+                action={async () => {
+                  'use server';
+                  await signIn('google', {redirectTo: "/"})
+                }}
+                className="flex items-center gap-1 border border-blue-500 
+                font-semibold bg-blue-50 px-4 py-1.5 hover:bg-blue-800
+                hover:text-white hoverEffect"
+                >
+                    <button className='flex gap-2 items-center'>
+                      Sign in with Google
+                      <FcGoogle />
+                    </button>
+                </form>
+                <form
+                  action={async () => {
+                    "use server";
+                    await signIn("google", { redirectTo: "/" });
+                  }}
+                  className="flex items-center gap-1 border border-slate-500 font-semibold bg-slate-50 px-2 py-1.5 hover:bg-slate-200 hoverEffect ease-in-out"
+                  >
+                    <FaGithub />
+                    <button type="submit">Signin with Github</button>
+                </form>
+            </div>
+          </div>
+          <SignInForm />
+       </div>
     </Container>
   )
 }
