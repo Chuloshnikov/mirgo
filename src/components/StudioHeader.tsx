76d6ce29486e759a67d92a00a11ba4f416237173
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-05-11 11:28:12 +0300

    +

--- src/components/StudioHeader.tsx
+++ src/components/StudioHeader.tsx
@@ -1,26 +1,25 @@
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 import { IoReturnDownBack } from 'react-icons/io5';
 import Logo from './Logo';
 
-// @ts-expect-error: Component props are not strictly typed
-const StudioHeader = (props) => {
+const StudioHeader = ({props}: any) => {
   return (
-    <div >
-        <div className='p-5 bg-accent text-gray-100 flex items-center justify-between'>
-            <Link 
-            href={"/"}
-            className='flex items-center gap-3 font-semibold hover:text-darkRed hoverEffect'
-            >
-                <IoReturnDownBack className='text-2xl'/>
-                Go to Website
-            </Link>
-            <Logo className='text-white'/>
-            <p className='hidden md:inline-flex text-sm'>Admin Studio for Mirago Online Shopping</p>
-        </div>
-        {props.renderDefault(props)}
+    <div>
+      <div className='p-5 bg-accent text-gray-100 flex items-center justify-between'>
+        <Link 
+          href="/"
+          className='flex items-center gap-3 font-semibold hover:text-darkRed hoverEffect'
+        >
+          <IoReturnDownBack className='text-2xl' />
+          Go to Website
+        </Link>
+        <Logo className='text-white' />
+        <p className='hidden md:inline-flex text-sm'>Admin Studio for Mirago Online Shopping</p>
+      </div>
+      {props.renderDefault(props)}
     </div>
-  )
-}
+  );
+};
 
 export default StudioHeader;
